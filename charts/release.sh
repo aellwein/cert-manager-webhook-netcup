ef313baa608e41184766384a8968b04a81d5afff
Alex Ellwein <alex.ellwein@gmail.com>
2026-04-24 09:24:48 +0200

    chore: remove obsolete self-hosted Helm chart repository

--- charts/release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-helm package ../deploy/cert-manager-webhook-netcup && \
-find . -name "*.tgz" | while read f; do \
-if [ ! -e "$f.prov" ]; then helm gpg sign $f ; helm gpg verify $f ; fi; done && \
-helm repo index .
